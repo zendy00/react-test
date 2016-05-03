a50746fd691b0b07230c208db67f456244c1c78e
Zendy Kim <Zendy Kim>
2016-05-03 15:46:19 +0900

    .

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var List = require('./components/List.jsx');
+
+ReactDom.render(<List />, document.getElementById('ingredients'));
